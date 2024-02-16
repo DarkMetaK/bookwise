bf1e1c25795f6c1222c805ffdeb13face4646d37
Darkmetak <matporto03@gmail.com>
2024-02-16 20:36:39 -0300

    Listagem dinâmica de livro por categoria

--- src/components/book-item.tsx
+++ src/components/book-item.tsx
@@ -5,10 +5,7 @@ interface IBookItemProps {
   id: string
   name: string
   author: string
-  summary: string
   cover_url: string
-  total_pages: number
-  created_at: Date
   ratings: {
     rate: number
   }[]
